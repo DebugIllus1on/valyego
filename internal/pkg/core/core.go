1af9fbfae5c0c2926e195fd6b51c9d8e1495477e
Lucas chen <lucas.chen@gbo-asia.com>
2023-02-15 15:42:19 +0800

    feat: 响应器

--- internal/pkg/core/core.go
+++ internal/pkg/core/core.go
@@ -2,11 +2,29 @@ package core
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/valyego/internal/pkg/errcode"
 )
 
+// ErrResponse 发生错误是返回的响应结构
+type ErrResponse struct {
+	Code    string `json:"code"`    // 业务代码
+	Message string `json:"message"` // 错误信息
+}
+
+// WriteResponse 错误写入到响应
 func WriteResponse(ctx *gin.Context, err error, data interface{}) {
 	if err != nil {
-		// httpcode, appcode, message := errcode.Decode(err)
-		// ctx.JSON(httpcode, )
+		httpcode, appcode, message := errcode.Decode(err)
+		ctx.JSON(httpcode, ErrResponse{
+			Code:    appcode,
+			Message: message,
+		})
+
+		return
 	}
+
+	ctx.JSON(errcode.OK.HTTP, ErrResponse{
+		Code:    errcode.OK.Code,
+		Message: errcode.OK.Message,
+	})
 }
--- internal/pkg/errcode/stdcode.go
+++ internal/pkg/errcode/stdcode.go
@@ -5,6 +5,5 @@ var (
 	InternalServerError = NewErrcode(200, "100101", "服务器内部错误") // InternalServerError 服务器错误
 	ErrResourceNotFound = NewErrcode(404, "100102", "对象不存在")
 	ErrInvalidParams    = NewErrcode(400, "100103", "无效的请求")
-
-	ErrCarNotFound = NewErrcode(404, "100201", "目标车辆不存在")
+	ErrCarNotFound      = NewErrcode(404, "100201", "目标车辆不存在")
 )
