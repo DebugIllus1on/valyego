c0b20450556486d030e6d5543681f6e8db41767c
Illus1onnn <1161524405@qq.com>
2023-02-15 23:56:04 +0800

    feat: 引入gorm库

---
+++ internal/zcar/model/user.go
@@ -0,0 +1,13 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"index;not null"`
+	Password string
+	Nickname string `gorm:"unique"`
+	Email    string `gorm:"type:varchar(100);unique"`
+	Mobile   string `gorm:"size:11;unique"`
+	Active   int    `gorm:"default:1"`
+}
