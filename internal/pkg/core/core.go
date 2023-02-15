b0ae415b256fecf6012485e358dbe66aa2519b76
Lucas chen <lucas.chen@gbo-asia.com>
2023-02-15 17:10:19 +0800

    fix: 不公开errResponse结构体

--- internal/pkg/core/core.go
+++ internal/pkg/core/core.go
@@ -5,8 +5,8 @@ import (
 	"github.com/valyego/internal/pkg/errcode"
 )
 
-// ErrResponse 发生错误是返回的响应结构
-type ErrResponse struct {
+// errResponse 发生错误是返回的响应结构
+type errResponse struct {
 	Code    string      `json:"code"`    // 业务代码
 	Message string      `json:"message"` // 错误信息
 	Data    interface{} `json:"data"`    // 数据
@@ -16,7 +16,7 @@ type ErrResponse struct {
 func WriteResponse(ctx *gin.Context, err error, data interface{}) {
 	if err != nil {
 		httpcode, appcode, message := errcode.Decode(err)
-		ctx.JSON(httpcode, ErrResponse{
+		ctx.JSON(httpcode, errResponse{
 			Code:    appcode,
 			Message: message,
 			Data:    nil,
@@ -25,7 +25,7 @@ func WriteResponse(ctx *gin.Context, err error, data interface{}) {
 		return
 	}
 
-	ctx.JSON(errcode.OK.HTTP, ErrResponse{
+	ctx.JSON(errcode.OK.HTTP, errResponse{
 		Code:    errcode.OK.Code,
 		Message: errcode.OK.Message,
 		Data:    data,
