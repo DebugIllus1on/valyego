3935cc5fecb70846453df9422dbd0dd7feed297d
Lucas chen <lucas.chen@gbo-asia.com>
2023-02-16 10:18:36 +0800

    feat: UserStore接口

--- internal/zcar/store/user.go
+++ internal/zcar/store/user.go
@@ -1,4 +1,44 @@
 package store
 
+import "github.com/jinzhu/gorm"
+
+// UserStore 定义了 user 在 Store 层需实现的方法
 type UserStore interface {
+	Get() error
+	List() error
+	Store() error
+	Update() error
+	Destroy() error
+}
+
+// user UserStore 接口的实现
+type user struct {
+	*gorm.DB
+}
+
+// newUser 实例化
+func newUser(dbc *gorm.DB) *user {
+	return &user{dbc}
+}
+
+var _ UserStore = (*user)(nil)
+
+func (u *user) Get() error {
+	return nil
+}
+
+func (u *user) List() error {
+	return nil
+}
+
+func (u *user) Store() error {
+	return nil
+}
+
+func (u *user) Update() error {
+	return nil
+}
+
+func (u *user) Destroy() error {
+	return nil
 }
