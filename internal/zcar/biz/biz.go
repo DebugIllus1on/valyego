58cf858c9fbc0d5160305423baa3aa188c10ef55
Lucas chen <lucas.chen@gbo-asia.com>
2023-02-16 17:41:37 +0800

    feat: biz逻辑层接口创建

--- internal/zcar/biz/biz.go
+++ internal/zcar/biz/biz.go
@@ -1,5 +1,22 @@
 package biz
 
+import "github.com/valyego/internal/zcar/store"
+
 type IBiz interface {
 	// TODO: 业务层接口实现
+	User()
+}
+
+var _ IBiz = (*biz)(nil)
+
+type biz struct {
+	ds store.IStore
+}
+
+func NewBiz(ds store.IStore) *biz {
+	return &biz{ds}
+}
+
+func (b *biz) User() {
+	return
 }
