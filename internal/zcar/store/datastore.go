1efbefb0cdf633593030d83799016fbba1edbe1d
Lucas chen <lucas.chen@gbo-asia.com>
2023-02-16 10:13:03 +0800

    feat: Store数据仓库

---
+++ internal/zcar/store/datastore.go
@@ -0,0 +1,33 @@
+package store
+
+import (
+	"sync"
+
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	once sync.Once
+	D    *datastore
+)
+
+// datastore 是 IStore 的具体实现
+type datastore struct {
+	dbc *gorm.DB
+}
+
+// NewStore 创建 IStore 实例
+func NewStore(dbc *gorm.DB) *datastore {
+	once.Do(func() {
+		D = &datastore{dbc}
+	})
+
+	return D
+}
+
+// 检查是否已经实现 IStore 
+var _ IStore = (*datastore)(nil)
+
+func (ds *datastore) User() UserStore {
+	return newUser(ds.dbc)
+}
--- internal/zcar/store/store.go
+++ internal/zcar/store/store.go
@@ -1,38 +1,6 @@
 package store
 
-import (
-	"sync"
-
-	"github.com/jinzhu/gorm"
-)
-
-var (
-	once sync.Once
-	DS   *datastore
-)
-
-// IStore 定义 Store 层实现的方法
+// IStore 定义需要实现的方法
 type IStore interface {
-	User() // User 用户
-}
-
-// datastore IStore 的具体实现
-type datastore struct {
-	db *gorm.DB
-}
-
-var _ IStore = (*datastore)(nil)
-
-// NewStore 创建一个 IStore 类型实例
-func NewStore(db *gorm.DB) *datastore {
-	once.Do(func() {
-		DS = &datastore{db}
-	})
-
-	return DS
-}
-
-// Users 返回实现 User 的实例
-func (ds *datastore) User() {
-	return
+	User() UserStore
 }
